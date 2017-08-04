9d13936c6cb6deeecad5fa588acaf49f264a6e60
Jeancasoto <jeancasoto@unitec.edu>
2017-08-04 14:39:38 -0600

    Minor fixes

--- lab3_progra2/src/lab3_progra2/Dragones.java
+++ lab3_progra2/src/lab3_progra2/Dragones.java
@@ -1,14 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package lab3_progra2;
 
-/**
- *
- * @author jeansoto
- */
-public class Dragones {
+
+public class Dragones extends Bestia{
+
+    private double long_ala;
+    
+    public Dragones() {
+    }
+
+    public Dragones(double long_ala) {
+        this.long_ala = long_ala;
+    }
+
+    public Dragones(double long_ala, int garras, Boolean veneno, int defensa, int ataque, int vida, int curacion) {
+        super(garras, veneno, defensa, ataque, vida, curacion);
+        this.long_ala = long_ala;
+    }
+
+    public double getLong_ala() {
+        return long_ala;
+    }
+
+    public void setLong_ala(double long_ala) {
+        this.long_ala = long_ala;
+    }
     
 }
